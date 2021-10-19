c2f7faa2f9df2dec2eb7b67ea5c7b16e4a8974e5
Breno Azevedo <maddox.br@gmail.com>
2021-10-19 00:49:14 -0300

    Added ScriptableObject-Architecture submodule

--- Assets/Scripts/SO_Scripts/TestSO.cs
+++ Assets/Scripts/SO_Scripts/TestSO.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
-using Candlelight;
+//using Candlelight;
 using UnityEngine;
 
 [CreateAssetMenu]
 public class TestSO : ScriptableObject
 {
-    [SerializeField, PropertyBackingField]
     //(typeof(RangeAttribute), 0f, 1f)] // override attribute type and its constructor args
-     
+
+    [SerializeField] //, PropertyBackingField]
     private float _FloatValue;
+
     public float FloatValue
     {
-        get { return _FloatValue; }
         set { _FloatValue = value; Debug.Log($"New value: {value}");}
+        get { return _FloatValue; }
     }
 }
